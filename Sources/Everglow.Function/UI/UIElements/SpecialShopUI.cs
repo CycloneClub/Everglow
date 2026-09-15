20688ef2d5c179dda794c4e5a194adec85e8a775
CloudeaSoft <cloudeasoft@qq.com>
2026-09-15 19:12:08 +0800

    refactor: isolate special shop registration and lifecycle

---
+++ Sources/Everglow.Function/UI/UIElements/SpecialShopUI.cs
@@ -0,0 +1,8 @@
+namespace Everglow.Commons.UI.UIElements;
+
+/// <summary>
+/// A shop registered alongside other UI containers. Open it through <see cref="SpecialShopRegistry.Open{T}"/>.
+/// </summary>
+public abstract class SpecialShopUI : UIContainerElement
+{
+}
