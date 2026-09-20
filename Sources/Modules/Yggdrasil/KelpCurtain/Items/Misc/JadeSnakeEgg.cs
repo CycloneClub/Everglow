90301ddc1fc788ccf45e17a7ff8c03691570ee77
unanmed <1319491857@qq.com>
2026-09-20 15:00:20 +0800

    feat(KelpCurtain): add JadeSnakeEgg with use disabled

---
+++ Sources/Modules/Yggdrasil/KelpCurtain/Items/Misc/JadeSnakeEgg.cs
@@ -0,0 +1,28 @@
+namespace Everglow.Yggdrasil.KelpCurtain.Items.Misc;
+
+public class JadeSnakeEgg : ModItem
+{
+	public override string LocalizationCategory => LocalizationUtils.Categories.SummonItems;
+
+	public override string Texture => Commons.ModAsset.White_Mod;
+
+	public override void SetDefaults()
+	{
+		Item.consumable = true;
+		Item.width = 20;
+		Item.height = 20;
+		Item.value = Item.buyPrice(gold: 10);
+		Item.rare = ItemRarityID.Blue;
+		Item.maxStack = Item.CommonMaxStack;
+		Item.useTime = Item.useAnimation = 12;
+		Item.noMelee = true;
+		Item.useTurn = true;
+		Item.UseSound = SoundID.Roar;
+		Item.useStyle = ItemUseStyleID.Swing;
+	}
+
+	public override bool CanUseItem(Player player)
+	{
+		return false;
+	}
+}
